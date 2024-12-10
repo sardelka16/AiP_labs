0b5a3d050d921cfcabfea4a58807bd169ee1bf1c
Andrei Leontev <166714565+sardelka16@users.noreply.github.com>
2024-12-11 00:25:45 +0600

    Лабораторная работа за 29.11.24

---
+++ laba 29.11.24/XYZamount.cs
@@ -0,0 +1,63 @@
+﻿//необходимо определить максимальную длину подпоследовательности, состоящую из троек элементов XYZ (последняя тройка может быть неполной)
+
+
+using System;
+class HelloWorld
+{
+
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        int max = 0;
+        int count = 0;
+        int lenght = s.Length;
+        for (int i = 0; i < lenght; i++)
+        {
+            if (s[i] == 'X')
+            { 
+                i++;
+                count++;
+                if (i < lenght && s[i] == 'Y')
+                { 
+                    i++;
+                    count++;
+                    if (i < lenght && s[i] == 'Z')
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        if (count > max)
+                        {
+                            max = count;
+                        }
+                        count = 0;
+                    } // если не Z
+
+                }
+                else
+                {
+                    if (count > max)
+                    {
+                        max = count;
+                    }
+
+                    count = 0;
+                } // если не Y
+            }
+            else
+            {
+                if (count > max)
+                {
+                    max = count;
+                }
+                count = 0;
+            } // если не X
+        }
+        if (count > max)
+        {
+            max = count;
+        }
+        Console.WriteLine(max);
+    }
+}
---
+++ laba 29.11.24/a_bCounter.cs
@@ -0,0 +1,60 @@
+﻿// на вход подаётся строка из латинскиих букв (могут быть и маленькие и большие, все одинаковые)
+//необходимо определить символ реже всего встречающийся в подстроке a*b, где * исходный символ (если таких символов несколько - выдать их все)
+
+using System;
+class HelloWorld
+{
+
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        s = s.ToLower();
+        string abba = ""; //строка со всеми звездочками
+        for (int i =1; i<=s.Length - 2; i++)
+        {
+            if ((s[i - 1] == 'a')&& (s[i + 1] == 'b'))
+            {
+                abba += s[i];
+            }
+        }//записали все звёздочки
+        int count = 1;
+        string amount = "";//строка в которой будут записаны количества всех букв
+        string letters = "";//строка в которой будут записаны буквы по очереди вхождения
+        while (abba != "")
+        {
+            for (int i=1; i<=abba.Length - 1; i++)
+            {
+                if (abba[0] == abba[i])
+                {
+                    count++;
+                }
+            }
+            amount += count.ToString();
+            letters += abba[0];
+            count = 1;
+            abba = abba.Replace(abba[0].ToString(), "");
+        }
+        Console.WriteLine(letters);
+        Console.WriteLine(amount);
+        if (letters == "")
+        {
+            Console.WriteLine("Нет подходящих символов");
+        }//Проверяем на наличие таких символов
+        int min = 100;
+        for (int i = 0; i<amount.Length; i++)
+        {
+            if (Convert.ToInt32(amount[i]) < min)
+            {
+                min= Convert.ToInt32(amount[i]);
+            }
+        }//Нашли минимум среди подходящих символов
+        for(int i=0;i<amount.Length; i++)
+        {
+            if(amount[i] == min)
+            {
+                Console.Write(letters[i] + " ");
+            }
+
+        }
+    }
+}
